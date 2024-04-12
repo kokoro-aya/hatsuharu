dea274a50eebcfdd83f455d655bff2da7d6d3757
kokoro-aya <35118912+kokoro-aya@users.noreply.github.com>
2024-04-12 15:28:08 +0200

    Resil: Reformatted test cases

--- src/test/scala/resil_visualizer/TestEvaluator.scala
+++ src/test/scala/resil_visualizer/TestEvaluator.scala
@@ -9,70 +9,78 @@ import moe.irony.resil.sig.Binary
 import moe.irony.resil.sig.Logical.LT
 
 class TestEvaluator extends munit.FunSuite:
-  test("a") {
-    val expr = Resil().eval ( If (Logop (Logical.LE, I (3), I (4)), I (3), I ( 2)))
-    assertEquals(expr, IntV(3))
+  test("simple if expr") {
+    val expr = If (Logop (Logical.LE, I (3), I (4)), I (3), I ( 2))
+    val res = Resil().eval(expr)
+    assertEquals(res, IntV(3))
   }
 
-  test("b") {
-    val expr = Resil().eval (Letrec(
+  test("simple binary(add) expr") {
+    val expr = Letrec(
       ResilEnv[RslExp](List(
         ("x", I(1)),
         ("y", I(2)),
         ("z", Binop(Binary.ADD, Var("x"), Var("y")))
       )),
-        Var("z")))
-    assertEquals(expr, IntV(3))
+        Var("z"))
+    val res = Resil().eval(expr)
+    assertEquals(res, IntV(3))
   }
 
-  test("c") {
-    val expr = Resil().eval (Call(Func("x", Binop(Binary.ADD, Var("x"), I(7))), I(1)))
-    assertEquals(expr, IntV(8))
+  test("simple function call") {
+    val expr = Call(Func("x", Binop(Binary.ADD, Var("x"), I(7))), I(1))
+    val res = Resil().eval(expr)
+    assertEquals(res, IntV(8))
   }
 
-  test("d") {
-    val expr = Resil().eval (Snd (Pair(I(7), I(2))))
-    assertEquals(expr, IntV(2))
+  test("snd of a pair") {
+    val expr = Snd (Pair(I(7), I(2)))
+    val res = Resil().eval(expr)
+    assertEquals(res, IntV(2))
   }
 
-  test("e") {
-    val expr = Resil().eval (Call (Func ("x", Binop(Binary.ADD, Var("x"), I(1))), Call(Func ("x", Binop(Binary.MULT, Var("x"), I(2))), I(3))))
-    assertEquals(expr, IntV(7))
+  test("nested func call") {
+    val expr = Call (Func ("x", Binop(Binary.ADD, Var("x"), I(1))), Call(Func ("x", Binop(Binary.MULT, Var("x"), I(2))), I(3)))
+    val res = Resil().eval(expr)
+    assertEquals(res, IntV(7))
   }
 
-  test("e1") {
-    val expr = Resil().eval (Letrec(
+  test("currying") {
+    val expr = Letrec(
       ResilEnv[RslExp](List(
         ("f", Func("x", Binop(Binary.ADD, Var("x"), I(1)))),
         ("g", Func("x", Binop(Binary.MULT, Var("x"), I(2)))),
         ("z", I(3))
       )),
-      Call(Var("f"), Call(Var("g"), Var("z")))))
-    assertEquals(expr, IntV(7))
+      Call(Var("f"), Call(Var("g"), Var("z"))))
+    val res = Resil().eval(expr)
+    assertEquals(res, IntV(7))
   }
 
-  test("f") {
-    val expr = Resil().eval(Letrec (
+  test("func as arg") {
+    val expr = Letrec (
       ResilEnv[RslExp](List(
         ("double", Func("f", Func("#x", Call(Var("f"), Call(Var("f"), Var("#x")))))),
         ("x", I(4))
       )),
-      Call(Call(Var("double"), Func("x", Binop(Binary.ADD, I(2), Var("x")))), Var("x"))))
-    assertEquals(expr, IntV(6))
+      Call(Call(Var("double"), Func("x", Binop(Binary.ADD, I(2), Var("x")))), Var("x")))
+    val res = Resil().eval(expr)
+    assertEquals(res, IntV(6))
   }
 
-  test("g") {
-    val expr = Resil().eval(Letrec(
+  test("simple func and letrec env var") {
+    val expr = Letrec(
       ResilEnv[RslExp](List(
         ("y", I(3)),
         ("f", Func("x", Binop(Binary.ADD, Var("y"), Var("x"))))
       )),
-      Call(Var("f"), I(4))))
-    assertEquals(expr, IntV(7))
+      Call(Var("f"), I(4)))
+    val res = Resil().eval(expr)
+    assertEquals(res, IntV(7))
   }
 
   test("forward references") {
-    val expr = Resil().eval(
+    val expr =
       Letrec(
         ResilEnv(
           List(
@@ -81,37 +89,40 @@ class TestEvaluator extends munit.FunSuite:
             ("h", Func("x", Call(Var("g"), Var("x"))))
           )),
         Call(Var("h"), AUnit())
-      ))
-
-    assertEquals(expr, IntV(2))
+      )
+    val res = Resil().eval(expr)
+    assertEquals(res, IntV(2))
   }
 
   test("stackoverflow in case of self-calls") {
-    val expr = Resil().eval(Letrec(
-      ResilEnv(
-        List(
-          ("f", Func("x", Call(Var("f"), Var("x"))))
-        )
-      ),
-      Call(Var("f"), AUnit())
-    ))
+    val expr =
+      Letrec(
+        ResilEnv(
+          List(
+            ("f", Func("x", Call(Var("f"), Var("x"))))
+          )
+        ),
+        Call(Var("f"), AUnit())
+      )
 
-    assertEquals(expr, ErrV("RangeError: Maximum call stack size exceeded"))
+    val res = Resil().eval(expr)
+    assertEquals(res, ErrV("RangeError: Maximum call stack size exceeded"))
   }
 
   test("self reduct to 10") {
-    val expr = Resil().eval(
+    val expr =
       Letrec(
-      ResilEnv(
-        List(
-          ("f", Func("x",
-            If(Logop(Logical.LT, Var("x"), I(10)),
-              Call(Var("f"), Binop(Binary.ADD, Var("x"), I(1))),
-              Var("x")
-          )))
-        )
-      ),
-      Call(Var("f"), I(1))))
+        ResilEnv(
+          List(
+            ("f", Func("x",
+              If(Logop(Logical.LT, Var("x"), I(10)),
+                Call(Var("f"), Binop(Binary.ADD, Var("x"), I(1))),
+                Var("x")
+            )))
+          )
+        ),
+        Call(Var("f"), I(1)))
 
-    assertEquals(expr, IntV(10))
+    val res = Resil().eval(expr)
+    assertEquals(res, IntV(10))
   }
